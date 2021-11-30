c5e1bc207988fb4ef0b37c6e5b6ba3cf91295470
Naveenajancy KB <81278043+Naveenajancy@users.noreply.github.com>
2021-11-30 11:17:03 -0500

    Update settings.py

--- AWSFileUpload/settings.py
+++ AWSFileUpload/settings.py
@@ -21,7 +21,7 @@ print(f"Base Dir : {BASE_DIR}")
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-1h0bx4abtn0k$kk5iejp(6p1hva0-6ul=y!5u#umxo8$j-j&+x'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
